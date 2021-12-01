3eb1103fef897e1fdb345daf248ddbb55314c68a
zydee3 <dev.zydea@gmail.com>
2021-11-30 22:25:01 -0800

    Re-added unit tests

---
+++ test/run_test_global.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Vincent Tan on 11/30/21.
+//
+
+#include "run_test_global.h"
+
+const void dappf::test::run_test_global::run() {
+
+}
---
+++ test/unit_test_handler.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Vincent Tan on 11/29/21.
+//
+
+#include "unit_test_handler.h"
+
+void dappf::test::unit_test_handler::test() {
+    dappf::data::handlers::handler(nullptr, nullptr, 0);
+}
